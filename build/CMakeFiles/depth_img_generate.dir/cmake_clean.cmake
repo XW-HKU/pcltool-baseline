dbc12b059483ee5ed0061efc30655b867ab88db0
melo_long <melolong@manifoldtech.cn>
2024-12-05 15:30:24 +0800

    多线程，无bug 1.自适应的filter阈值 2.满足map点与临近像素点的角度差值小于0.3度

---
+++ build/CMakeFiles/depth_img_generate.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/depth_img_generate.dir/LX_tools/lx_tool.cpp.o"
+  "CMakeFiles/depth_img_generate.dir/LX_tools/nerf_pre.cpp.o"
+  "CMakeFiles/depth_img_generate.dir/PointCloudCart2Sph/PointCloudCart2Sph.cpp.o"
+  "CMakeFiles/depth_img_generate.dir/SmoothPointCloud/SmoothPointCloud.cpp.o"
+  "CMakeFiles/depth_img_generate.dir/depth_img_generate.cpp.o"
+  "depth_img_generate"
+  "depth_img_generate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/depth_img_generate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/voxel_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel_filter.dir/voxel_filter.cpp.o"
+  "voxel_filter"
+  "voxel_filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxel_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
